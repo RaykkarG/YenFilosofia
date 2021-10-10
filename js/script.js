424dd787b1856b97a63b19ff682ff5c1f91d6f50
Raykkar Garney <ray-garney@hotmail.com>
2021-10-10 17:55:48 -0300

    Yen Filosofia - primeiro commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+new SimpleSlide({
+    slide:'quote', // nome do atributo data-slide="principal"
+    time: 7000 // tempo de transição dos slides
+  });
+
+  new SimpleSlide({
+    slide:'arte', // nome do atributo data-slide="principal"
+    time: 7000, // tempo de transição dos slides
+    nav: false,
+  });
